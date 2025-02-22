a755eb3d87b654c3032531d7ae479c74a0f097e7
Mike Clancy <mclancy96@gmail.com>
2025-02-22 08:48:01 -0500

    copy over index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+$(window).on("load", () => {
+    $(window).scroll(() => {
+        let windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".scroll-fade").each(function () {
+            let objectTop = $(this).offset().top;
+            if (objectTop < windowBottom) {
+                if ($(this).css("opacity") == 0) {
+                    $(this).fadeTo(1000, 1);
+                }
+            }
+            // else {
+            //     if ($(this).css("opacity")==1) {
+            //         $(this).fadeTo(500,0);
+            //     }
+            // }
+        });
+    }).scroll();
+});
